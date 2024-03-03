5e7c856815277ccdf27023209f4765add8ba2c25
陳宗興 <barrydog505@gmail.com>
2024-03-03 21:43:58 +0800

    test

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
 import torch
 
-a = torch.tensor([1, 2, 3])
-b = torch.tensor([4, 5, 6])
-
-# a = torch.tensor([[1], [2], [3]])
-# b = torch.tensor([[4], [5], [6]])
-
-print(f'a shape: {a.shape}')
-print(f'b shape: {b.shape}')
-
-# print(torch.cat((a, b), dim=1).size())
-print(torch.vstack((a, b)).size())
+for i in range(0):
+    print(i)
