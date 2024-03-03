3a8063976e3e146bc8c235be5aef7c7b0fe5786f
陳宗興 <barrydog505@gmail.com>
2024-03-03 21:44:12 +0800

    finish

--- random_feature_method/Helmholtz.py
+++ random_feature_method/Helmholtz.py
@@ -5,10 +5,19 @@ torch.set_default_dtype(torch.float64)
 
 
 def exact_solution(x):
-    return torch.sin(2 * torch.pi * x)
+    func = (
+        torch.sin(3.0 * torch.pi * (x + 0.05)) * 
+        torch.cos(2.0 * torch.pi * (x + 0.05))
+        + 2.0
+    )
+    return func
+
 
 def rhs_function(x):
-    return - 4 * torch.pi ** 2 * torch.sin(2 * torch.pi * x)
+    u_xx = torch.vmap(torch.func.grad(torch.func.grad(
+        exact_solution, argnums=0), argnums=0),
+        in_dims=0, out_dims=0)(x.reshape(-1))
+    return u_xx.reshape(-1, 1) - 4 * exact_solution(x)
 
 
 class Model:
@@ -16,8 +25,8 @@ class Model:
         self.M_p = M_p
 
     def mapping(self, x, idx):
-        xn = (2 * (idx + 1) - 1) / (2 * self.M_p)
-        rn = 1 / (2 * self.M_p)
+        xn = 8 * (2 * (idx + 1) - 1) / (2 * self.M_p)
+        rn = 8 / (2 * self.M_p)
         return (x - xn) / rn
     
     def PoU(self, x):
@@ -72,8 +81,10 @@ class Model:
             # ax.scatter(points[i], self.mapping(points[i], i))
             # plt.show()
 
+            Lu = grad2 - 4 * values
+
             # Assemble the PDE term
-            A_I[i * Q : (i + 1) * Q, i * J_n : (i + 1) * J_n] = grad2[1:-1, :]
+            A_I[i * Q : (i + 1) * Q, i * J_n : (i + 1) * J_n] = Lu[1:-1, :]
             f[i * Q : (i + 1) * Q, :] = rhs_function(points[i])[1:-1, :]
         
             # Boundary conditions
@@ -83,7 +94,7 @@ class Model:
                 A_B[1, -J_n:] = value_r
 
             # Smoothness conditions
-            if self.M_p > 1:
+            if self.M_p > 0:
                 if i == 0:
                     A_C_0[0, :J_n] = -value_r
                     A_C_1[0, :J_n] = -grad1_r
@@ -103,10 +114,11 @@ class Model:
 
         # RHS boundary conditions
         f[self.M_p * Q, :] = exact_solution(torch.tensor(0.0))
-        f[self.M_p * Q + 1, :] = exact_solution(torch.tensor(1.0))
+        f[self.M_p * Q + 1, :] = exact_solution(torch.tensor(8.0))
 
         # print(f'A: {A.shape}')
         # print(f'f: {f.shape}')
+
         return A, f
     
 
@@ -133,7 +145,7 @@ def test_model(model, weights, biases, points, J_n, Q, u_hat):
     numerical_values = torch.tensor(numerical_values).reshape(-1, 1)
 
     fig, axs = plt.subplots(2, 1)
-    axs[0].scatter(plot_points, exact_values, label="Exact solution", color="red")
+    axs[0].scatter(plot_points, exact_values, label="Exact solution", color="red", marker="o")
     axs[0].scatter(plot_points, numerical_values, label="Numerical solution", color="blue", marker=".")
     axs[1].scatter(plot_points, error, label="Error", color="green", marker=".")
     axs[0].legend()
@@ -144,10 +156,10 @@ def test_model(model, weights, biases, points, J_n, Q, u_hat):
 
 
 def main():
-    J_n = 50  # the number of basis functions per PoU region
-    Q = 50  # the number of collocation pointss per PoU region
-    M_p = 4  # the number of PoU regions
-    R_m = 5  # the range of the random weights and biases
+    J_n = 100  # the number of basis functions per PoU region
+    Q = 100  # the number of collocation pointss per PoU region
+    M_p = 8  # the number of PoU regions
+    R_m = 3  # the range of the random weights and biases
 
     # Initialize the weights and biases
     weights = torch.randn(1, 1, J_n, M_p).uniform_(-R_m, R_m)
@@ -155,7 +167,7 @@ def main():
 
     # Create the training and tset points data
     X_min = torch.tensor(0.0)
-    X_max = torch.tensor(1.0)
+    X_max = torch.tensor(8.0)
     training_points = []
     test_points = []
     for i in range(M_p):
