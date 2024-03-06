ff5c53c256dd0dff981cadab0b6cf9ab997a0282
陳宗興 <barrydog505@gmail.com>
2024-03-07 01:26:56 +0800

    temp

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ x = torch.randint(1, 10, (5, 2), dtype=torch.float64)
 jac = jacrev(f)(x).squeeze()
 grad = torch.cos(x[:, 0]) * torch.sin(x[:, 1])
 
-# jac2 = vmap(jacrev(jacrev(f)), in_dims=0, out_dims=0)(x).squeeze()
+jac2 = vmap(jacrev(jacrev(f)), in_dims=0, out_dims=0)(x).squeeze()
 # grad2 = -torch.sin(x)
 
 print(f'x = \n{x}')
