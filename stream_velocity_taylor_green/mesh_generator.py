56d1a2e59956b69cc741fce87faf5b7cc992b9c3
陳宗興 <barrydog505@gmail.com>
2024-04-09 14:06:30 +0800

    add circle mesh

--- stream_velocity_taylor_green/mesh_generator.py
+++ stream_velocity_taylor_green/mesh_generator.py
@@ -1,10 +1,11 @@
 import torch
+import numpy as np
 from scipy.stats import qmc
 
 torch.set_default_dtype(torch.float64)
 
 
-class CreateMesh:
+class CreateSquareMesh:
     def inner_points(self, nx):
         points = torch.from_numpy(qmc.LatinHypercube(d=2).random(n=nx))
         points_x = points[:, 0].reshape(-1, 1)
@@ -15,14 +16,14 @@ class CreateMesh:
         left_bd_x = torch.full((nx, 1), 0.0)
         right_bd_x = torch.full((nx, 1), 1.0)
         top_bd_x = torch.vstack((
-            torch.tensor(0.0), torch.from_numpy(qmc.LatinHypercube(d=1).random(n=nx)), torch.tensor(1.0)))
+            torch.tensor(0.0), torch.from_numpy(qmc.LatinHypercube(d=1).random(n=nx-2)), torch.tensor(1.0)))
         bottom_bd_x = torch.vstack((
-            torch.tensor(0.0), torch.from_numpy(qmc.LatinHypercube(d=1).random(n=nx)), torch.tensor(1.0)))
+            torch.tensor(0.0), torch.from_numpy(qmc.LatinHypercube(d=1).random(n=nx-2)), torch.tensor(1.0)))
 
         left_bd_y = torch.vstack((
-            torch.tensor(0.0), torch.from_numpy(qmc.LatinHypercube(d=1).random(n=nx)), torch.tensor(1.0)))
+            torch.tensor(0.0), torch.from_numpy(qmc.LatinHypercube(d=1).random(n=nx-2)), torch.tensor(1.0)))
         right_bd_y = torch.vstack((
-            torch.tensor(0.0), torch.from_numpy(qmc.LatinHypercube(d=1).random(n=nx)), torch.tensor(1.0)))
+            torch.tensor(0.0), torch.from_numpy(qmc.LatinHypercube(d=1).random(n=nx-2)), torch.tensor(1.0)))
         top_bd_y = torch.full((nx, 1), 1.0)
         bottom_bd_y = torch.full((nx, 1), 0.0)
 
@@ -43,4 +44,32 @@ class CreateMesh:
 
         nx = torch.vstack((nx_left, nx_right, nx_top, nx_bottom))
         ny = torch.vstack((ny_left, ny_right, ny_top, ny_bottom))
+        return nx, ny
+
+
+class CreateCircleMesh:
+    def __init__(self, *, xc=0, yc=0, r=1):
+        self.xc = xc
+        self.yc = yc
+        self.r = r
+
+    def inner_points(self, nx):
+        radius = torch.tensor(self.r * np.sqrt(qmc.LatinHypercube(d=1).random(n=nx)))
+        theta = torch.tensor(2 * np.pi * qmc.LatinHypercube(d=1).random(n=nx))
+        points_x = self.xc + radius * torch.cos(theta)
+        points_y = self.yc + radius * torch.sin(theta)
+        return points_x, points_y
+
+    def boundary_points(self, nx):
+        theta = torch.tensor(2 * np.pi * qmc.LatinHypercube(d=1).random(n=nx))
+        points_x = self.xc + self.r * torch.cos(theta)
+        points_y = self.yc + self.r * torch.sin(theta)
+        return points_x, points_y
+
+    def normal_vector(self, x, y):
+        nx = 2 * x / 1
+        ny = 2 * y / 1
+        dist = torch.sqrt(nx**2 + ny**2)
+        nx = nx / dist
+        ny = ny / dist
         return nx, ny
