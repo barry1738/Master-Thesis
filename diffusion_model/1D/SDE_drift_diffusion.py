71da53e81101f62b18ef31c562cc4739351fd2f8
barry1738 <barrydog505@gmail.com>
2024-07-24 22:45:32 +0800

    Finish 1D SDE

---
+++ diffusion_model/1D/SDE_drift_diffusion.py
@@ -0,0 +1,68 @@
+''' 
+1D forward stochastic process
+dXt  = mu*dt + sigma*dWt
+X(0) = X0 (const.)
+------------------------- 
+mean = X0 + mu*t
+var  = sigma^2*t
+------------------------- 
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+# numerical setup
+T = 2  # terminal time
+M = 100  # number of iterations
+dt = T / M  # time step size
+N = 2000  # number of particles
+# ... parameters in f and g ...
+mu = 3
+sigma = 0.5
+# ... initial condition ...
+X_0 = 0
+# ... exact mean and std at T ...
+mu_ex = X_0 + mu * T
+std_ex = sigma * np.sqrt(T)
+
+# SDE setup
+def f(x, t):
+    return mu
+
+def g(x, t):
+    return sigma
+
+# Euler-Maruyama method
+Xh_0 = np.zeros((N, M + 1))
+Xh_0[:, 0] = X_0
+
+for i in range(M):
+    ti = i*dt
+    Xh_0[:, i+1] = Xh_0[:, i] \
+                 + f(Xh_0[:, i], ti) * dt \
+                 + g(Xh_0[:, i], ti) * np.sqrt(dt) * np.random.randn(N)
+
+# Compute mean and std from discrete data
+mu_sde = np.mean(Xh_0[:,-1])
+std_sde = np.sqrt(np.sum((Xh_0[:, -1] - mu_sde) ** 2) / N)
+
+print("--------------------")
+print(f"Exact mean: {mu_ex:.6f}")
+print(f"Numer.mean: {mu_sde:.6f}")
+print("--------------------")
+print(f"Exact std : {std_ex:.6f}")
+print(f"Numer.std : {std_sde:.6f}")
+print("--------------------")
+
+# Output
+fig, axs = plt.subplots(1, 2, figsize=(8, 6))
+axs[0].plot(np.linspace(0, T, M + 1), Xh_0.T)
+axs[0].set_ylim([-10, 10])
+axs[0].set_xlabel('time')
+axs[0].set_ylabel('X(t)')
+
+axs[1].plot(-0.1*np.ones_like(Xh_0[:, -1]), Xh_0[:, -1], 'k.')
+axs[1].plot(norm.pdf(np.arange(-10, 10, 0.1), mu_ex, std_ex), np.arange(-10, 10, 0.1))
+axs[1].set_axis_off()
+plt.show()
---
+++ diffusion_model/1D/SDE_reverse_OU.py
@@ -0,0 +1,78 @@
+"""
+1D reverse Ornstein-Uhenbeck process
+dXt  = -beta*Xt*dt + sigma*dWt
+X(0) = X0 ~ N(mu_0, sigma_0^2)
+------------------------------
+mean = mu_0*exp(-beta*t)
+var  = sigma_0^2*exp(-2*beta*t) + sigma^2/(2*beta)*( 1 - exp(-2*beta*t) )
+------------------------------
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+# numerical setup
+T  = 2    # terminal time
+M  = 100  # number of iterations
+dt = T / M  # time step size
+N  = 2000 # number of particles
+# ... parameters in f and g ...
+beta = 1
+sigma = 0.5
+# ... exact mean and std at t = 0 ...
+mu_0    = 2
+sigma_0 = 2
+# ... initial condition ...
+mu  = lambda t: mu_0 * np.exp(-beta * t)  # noqa: E731
+std = lambda t: np.sqrt(  # noqa: E731
+    np.exp(-2 * beta * t) * sigma_0**2
+    + sigma**2 / (2 * beta) * (1 - np.exp(-2 * beta * t))
+)
+X_0 = np.random.normal(mu(T), std(T), N)
+
+# Ornstein-Uhenbeck process
+def f(x, t):
+    return -beta * x
+
+def g(x, t):
+    return sigma
+
+def s(x, t):
+    return -(x - mu(t)) / std(t) ** 2
+
+# Euler-Maruyama method (backward)
+Xh_0 = np.zeros((N, M + 1))
+Xh_0[:, -1] = X_0
+
+for i in range(M, 0, -1):
+    print(i)
+    ti = i * dt
+    Xh_0[:, i-1] = Xh_0[:, i] \
+                 + (f(Xh_0[:, i],ti) - g(Xh_0[:, i],ti)**2 * s(Xh_0[:, i],ti)) * (-dt) \
+                 + g(Xh_0[:, i],ti) * np.sqrt(dt) * np.random.randn(N)
+
+# Compute mean and std from discrete data
+mu_sde = np.mean(Xh_0[:, 0])
+std_sde = np.sqrt(np.sum((Xh_0[:, 0] - mu_sde) ** 2) / N)
+
+print("--------------------")
+print(f"Exact mean: {mu_0:.6f}")
+print(f"Numer.mean: {mu_sde:.6f}")
+print("--------------------")
+print(f"Exact std : {sigma_0:.6f}")
+print(f"Numer.std : {std_sde:.6f}")
+print("--------------------")
+
+# Output
+fig, axs = plt.subplots(1, 2, figsize=(8, 6))
+axs[0].plot(np.linspace(0, T, M + 1), Xh_0.T)
+axs[0].invert_xaxis()
+axs[0].set_ylim([-10, 10])
+axs[0].set_title('Reverse')
+axs[0].set_xlabel('time')
+axs[0].set_ylabel(r'$\bar{X}(t)$')
+axs[1].plot(-0.1*np.ones_like(Xh_0[:, 0]), Xh_0[:, 0], 'k.')
+axs[1].plot(norm.pdf(np.arange(-10, 10, 0.1), mu_0, sigma_0), np.arange(-10, 10, 0.1))
+axs[1].set_axis_off()
+plt.show()
