ce07bd4cef8d9a9d73042aae66af464551eeaf0a
陳宗興 <barrydog505@gmail.com>
2024-08-10 03:10:33 +0800

    update

--- diffusion_model/DDPM/MNIST/SDE_reverse_DDPM.py
+++ diffusion_model/DDPM/MNIST/SDE_reverse_DDPM.py
@@ -10,6 +10,7 @@ var  = 1 - bar{alpha}_k
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 # numerical setup
@@ -32,7 +33,8 @@ sigma_tilde = np.append(
 X_0 = np.loadtxt("diffusion_model/DDPM/MNIST/X_0.csv").reshape(-1, 1)
 mu_ex = np.zeros((d, 1))
 std_ex = 1
-Xh_0 = np.random.multivariate_normal(mu_ex.flatten(), std_ex**2 * np.eye(d), N).T
+Xh_0 = np.zeros((d, M))
+Xh_0[:, -1] = np.random.multivariate_normal(mu_ex.flatten(), std_ex**2 * np.eye(d), N).flatten()
 
 # SDE setup
 mu = lambda t: np.exp(-(t ** 2) / 4) * X_0
@@ -40,31 +42,41 @@ std = lambda t: np.sqrt(1 - np.exp(-t ** 2 / 2))
 s = lambda x, t: -(x - mu(t)) / std(t) ** 2
 eps = lambda x, t: -std(t) * s(x, t)
 
-# Euler-Maruyama method (backward)
-for i in range(M - 1, 0, -1):
-    ti = i * dt
-    Xh_0 = 1 / np.sqrt(alpha[i]) * (
-        Xh_0 - (1 - alpha[i]) / np.sqrt(1 - alpha_bar[i]) * eps(Xh_0, ti)
-    ) + sigma_tilde[i] * np.random.randn(d, N)
+
+def init():
+    ax.axis("off")
+
+
+def update(frame):
+    ax.clear()
+    # Euler-Maruyama method (backward)
+    ti = frame * dt
+    Xh_0[:, frame - 1] = (
+        1
+        / np.sqrt(alpha[frame])
+        * (
+            Xh_0[:, frame].reshape(-1, 1)
+            - (1 - alpha[frame])
+            / np.sqrt(1 - alpha_bar[frame])
+            * eps(Xh_0[:, frame].reshape(-1, 1), ti)
+        )
+        + sigma_tilde[frame] * np.random.randn(d, N)
+    ).flatten()
+
+    ax.imshow(Xh_0[:, frame].reshape(28, 28).T, cmap="gray")
+    ax.set_title(f"t = {frame}")
+    ax.axis("off")
 
 
 fig, ax = plt.subplots()
-ax.imshow(Xh_0.reshape(28, 28).T, cmap="gray")
-ax.axis("off")
-plt.show()
+ani = animation.FuncAnimation(
+    fig, update, frames=range(M - 1, 0, -1), interval=1, init_func=init, repeat=False
+)
+ani.save("C:\\Users\\barry\\Desktop\\MNIST.gif", writer="pillow", fps=100)
 
 # Compute mean and std from discrete data
-mu_sde = np.sum(Xh_0, axis=1) / N
-cov_sde = np.cov(Xh_0.T) * (1 - 1 / N)
-
-# Output
-# print("------------------------")
-# print(f"Exact.mean = \n{X_0}")
-# print(f"Numer.mean = \n{mu_sde}")
-# print("------------------------")
-# print(f"Exact.cov = \n{0*np.eye(d)}")
-# print(f"Numer.cov = \n{cov_sde}")
-# print("------------------------")
+mu_sde = Xh_0[:, 0]
+cov_sde = np.cov(Xh_0[:, 0].T) * (1 - 1 / N)
 
 norm = np.linalg.norm(mu_sde - X_0.flatten(), ord=np.inf)
 print(f"norm of difference in mean = {norm:.6f}")
